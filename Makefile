656640cb8086971a4a7c15ff12d3631ea4c7653c
Chris Crook <ccrook@linz.govt.nz>
2023-07-27 09:18:01 +1200

    Revision r1 updates
    Revision r1 of the Abstract Specification.  This includes:
    
    * amendments from OGC editorial review
    * amendments from public review
    * inclusion of "Topic 24" in the document title
    * inserting Annex A, conformance requirements
    * breaking Clause 5 into multiple sections rather than a numbered list for ease of referencing
    * inserting Clause 6.1.3 defined how deformation parameter are evaluated for an element when they are not parameters of the spatial function grids
    * replacement of logarithmic time function with logBaseE and logBase10 functions
    * correction to cyclic time function formula
    * numerous editorial and grammatical fixes

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 specification:
-	scripts/compile_specification.sh
+	metanorma compile --agree-to-terms -t ogc -x xml,pdf,html,doc products/specification/abstract-specification-deformation-model-functional-model.adoc
--- scripts/get_spec_template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Script to extract the abstract specification template from the metanorma docker image.  This is run once only -
-# just included here for the record as to how it was done.
-#
-docker run --rm -v "$(pwd)":/metanorma metanorma/mn metanorma new -d abstract-specification-topic -t ogc -l https://github.com/metanorma/mn-templates-ogc temp/specification
-sudo chown -R "$(id -u):$(id -g)" .
